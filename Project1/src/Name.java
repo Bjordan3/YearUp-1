a185d51ad8361049ece76c1b357f0e599d4610cc
Jacquawn <Jlockhart@atl.yearup.org>
2015-09-24 09:49:26 -0400

    Update 9/24/15

--- Project1/src/Name.java
+++ Project1/src/Name.java
@@ -6,14 +6,11 @@ public class Name {
 	public static void main(String args[]) throws IOException {
 
 		System.out.println("Hello and welcome, Please follow the directions.");
-
 		Scanner s = new Scanner(System.in);
-
 		String name = "";
-		String yn  = "";
+		String yn = "";
 		int age = 0;
 		double userHeight = 0;
-		
 
 		System.out.println("What is your name?: ");
 		name = s.nextLine();
@@ -21,20 +18,23 @@ public class Name {
 		age = s.nextInt();
 		System.out.println("What is your height in inches?: ");
 		userHeight = s.nextDouble();
-
 		System.out.println("Hello " + name + ". You have told me your age is " + age + " your height is " + userHeight
 				+ "inches tall. Nice to meet you, my name is Cortona.");
-		System.out.println("Want to hear a joke, Y for yes and N for No: ");
 		
-	if(yn == "y"){
-		yn = s.nextLine();
-		System.out.println("Where is a computers love stored?: ");
-		System.in.read();
-		System.out.println("The motherboard!!");
-		}
-	else if(yn == "n"){
-		System.out.println("You have fried my circuits!");
+		Add();
+		Add(10,20);
 	}
+	public static void Add(){
+		int a = 3;
+		int b = 5;
+		int sum = (a + b);
+		System.out.println("The numbers were 3 and 5. When they are add they are " + sum + ".");
+	}
+	
+	public static void Add(int c, int d){
+		int sum2 = c + d;
+		System.out.println( "The sum is of " + c + " and " + d + " is " + sum2 + ".");
+		
 	}
 
 }
