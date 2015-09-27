6d836e6d6eb38eef362925c7e6594265a14d76a2
Jacquawn <Jlockhart@atl.yearup.org>
2015-09-27 01:01:21 -0400

    I have added the percentage code to the project

--- Project1/src/Name.java
+++ Project1/src/Name.java
@@ -6,28 +6,49 @@ public class Name {
 	public static void main(String args[]) throws IOException {
 
 		System.out.println("Hello and welcome, Please follow the directions.");
+// scanner that scans the next input
 		Scanner s = new Scanner(System.in);
+// declares a data type string and leaves its value empty 		
 		String name = "";
-		String yn = "";
+// declares a data type int and makes it zero
 		int age = 0;
+// declares a data type double and makes it zero
 		double userHeight = 0;
-
+//Prints saves the input of name in a string and the height and age in an int variable
 		System.out.println("What is your name?: ");
 		name = s.nextLine();
 		System.out.println("What is your age?: ");
 		age = s.nextInt();
 		System.out.println("What is your height in inches?: ");
 		userHeight = s.nextDouble();
-		System.out.println("Hello " + name + ". You have told me your age is "
+// prints all the stored values		
+		System.out.println("Hello " + name + ". You have told me your age is"
 				+ age + " your height is " + userHeight
 				+ "inches tall. Nice to meet you, my name is Cortona.");
-
+// calls the methods of Add with parameters and Add without parameters and displays them
 		Add();
-		Add(38, 27);
-		double AreaInMainMethodC = CalculateCircleArea(12);
-		double AreaInMaindMethodR = CalculateRectangleArea(7, 8);
-		System.out.println("The area of the circle is " + AreaInMainMethodC + ", and the area of the rectangle is " + AreaInMaindMethodR + ".");
-		
+		Add(10, 20);
+// states a data type to store the values of the areas from the circle and rectangle methods 
+		double AreaInMainMethodC = CalculateCircleArea(5);
+		double AreaInMaindMethodR = CalculateRectangleArea(1, 2);
+// prints the stored values of the areas of the circle and rectangle
+		System.out.println("The area of the circle is "
+				+ AreaInMainMethodC + ", and the area of the rectangle is "
+				+ AreaInMaindMethodR + ".");
+// ask for a bill amount
+		System.out.println("What is bill amount?: ");
+// declares the double amount 
+		double amt;
+// stores the next amount in the amt variable
+		amt = s.nextFloat();
+// calls all the methods to have the amt stored from the input into them and be executed
+		double tenPercent = TenPercent(amt);
+		double fifteenPercent = FifteenPercent(amt);
+		double twentyPercent = TwentyPercent(amt);
+// Prints out the bill amounts with the 10 15 and 20 percent amounts added on
+		System.out.println("You tips are as follow. 10%: " + tenPercent
+				+ " 15% " + fifteenPercent + " and 20% " + twentyPercent + ".");
+
 	}
 
 	public static void Add() {
@@ -53,17 +74,31 @@ public class Name {
 				+ ", and this string is done! ";
 
 		System.out.println(str);
-	
-	}
-	
-	public static double CalculateCircleArea(int radius){
-		double area = 3.14*radius*radius;
-		return area;
-	}
-	public static double CalculateRectangleArea(int length, int width){
-		double area = length * width;
-		return area;
-		
+
 	}
 
+	public static double CalculateCircleArea(int radius) {
+		double area = 3.14 * radius * radius;
+		return area;
+	}
+	public static double CalculateRectangleArea(int length, int width) {
+		double area = length * width;
+		return area;
+
+	}
+	public static double TenPercent(double amt) {
+
+		double TenPercent = ((amt * .1) + amt);
+		return TenPercent;
+	}
+	public static double FifteenPercent(double amt) {
+
+		double fifteenPercent = ((amt * .15) + amt);
+		return fifteenPercent;
+	}
+	public static double TwentyPercent(double amt) {
+
+		double TwentyPercent = ((amt * .20) + amt);
+		return TwentyPercent;
+	}
 }
