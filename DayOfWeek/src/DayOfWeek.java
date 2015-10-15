ebcf8667d858cb27f494cce7d862e87146a34125
Jacquawn <Jlockhart@atl.yearup.org>
2015-10-15 08:03:42 -0400

    10/15/15

--- DayOfWeek/src/DayOfWeek.java
+++ DayOfWeek/src/DayOfWeek.java
@@ -3,33 +3,71 @@ public class DayOfWeek {
 
 	public static void main(String[] args) {
 		Scanner a = new Scanner(System.in);
-		System.out.println("Enter a new day of week:\n");
-		String day = null;
-		day = a.next();
 		
-		if (day.equals("Monday") || (day.equals("Tuesday"))
-				|| (day.equals("Wednesday")) || (day.equals("Thursday"))
-				|| (day.equals("Friday"))) {
-			System.out.println("You have to go to work. ");
-		} else if (day.equals("Saturday") || (day.equals("Sunday"))) {
-			System.out.println("Take the Day off");
-		} else {
-			System.out.println("That is not a valid day");
+//		// do while loop for evaluating the day of the week
+//		System.out.println("Enter a new day of week or Quit to exit:");
+//		String day = null;
+//		day = a.nextLine();
+//		do{
+//		EvaluateDayOfWeek(day);
+//		}
+//		 while (!day.equals("Quit")); 
+		
+		//do while loop for switch statements
+		
+		String day = "Start";
+		day = a.nextLine();
+		 do {
+		 System.out.println("Enter a new day of week or Quit to exit:");
+		 day = a.nextLine();
+		 switch (day) {
+		 case "Monday" :
+		 EvaluateDayOfWeek(day);
+		 break;
+		 case "Tuesday" :
+		 EvaluateDayOfWeek(day);
+		 break;
+		 case "Wednesday" :
+		 EvaluateDayOfWeek(day);
+		 break;
+		 case "Thursday" :
+		 EvaluateDayOfWeek(day);
+		 break;
+		 case "Friday" :
+		 EvaluateDayOfWeek(day);
+		 break;
+		 case "Saturday" :
+		 EvaluateDayOfWeek(day);
+		 break;
+		 case "Sunday" :
+		 EvaluateDayOfWeek(day);
+		 break;
+		 case "" :
+		 EvaluateDayOfWeek(day);
+		 break;
+		 }
+		 } while (!day.equals("Quit"));
+	}
 
+	public static void EvaluateDayOfWeek(String day) {
+
+		if (!day.isEmpty()) {
+
+			if (day.equals("Monday") || (day.equals("Tuesday"))
+					|| (day.equals("Wednesday")) || (day.equals("Thursday"))
+					|| (day.equals("Friday"))) {
+				System.out.println("You have to go to work. ");
+			} else if (day.equals("Saturday") || (day.equals("Sunday"))) {
+				System.out.println("Take the Day off");
+			} else {
+				System.out.println("That is not a valid day");
+			}
+		}
+
+		else {
+			System.out.println("You didnt enter anything please try again.");
 		}
 
 	}
-	public static void EvaluateDayOfWeek(String DayOfWeek) {
-		Scanner b = new Scanner(System.in);
-		DayOfWeek = b.nextLine();
 
-		if (DayOfWeek.equals(null)) {
-			System.out.println("This is empty. Please try again");
-		}
-
-	}
-	public String IsEmpty() {
-		String ne = "This is not empty";
-		return ne;
-	}
 }
